0900c8bea07ee16b40e732bfb6e6133834a2d182
Alma Isaksson <isakvent@outlook.com>
2024-05-22 12:17:54 +0000

    add display partnering end and product

--- serial_number/serializers.py
+++ serial_number/serializers.py
@@ -9,6 +9,8 @@ class SerialNumberSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     user_authentication_id = serializers.ReadOnlyField(source='owner.user_authentication.id')
     updated_at = serializers.SerializerMethodField()
+    display_link_product_name = serializers.CharField(source='link_product_name.product_name', read_only=True)
+    display_link_partnering_end = serializers.CharField(source='link_partnering_end.partnering_end', read_only=True)
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -29,4 +31,6 @@ class SerialNumberSerializer(serializers.ModelSerializer):
             'user_authentication_id',
             'updated_at',
             'serial_number',
+            'display_link_product_name',
+            'display_link_partnering_end',
         ]
